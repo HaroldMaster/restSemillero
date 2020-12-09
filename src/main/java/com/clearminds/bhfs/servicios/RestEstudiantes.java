5e9c381442f0ac48e82d1c0c177685265b8aa213
Harold Flores <bhflores1994@gmail.com>
2020-12-09 14:14:40 -0500

    Se añaden los metodos insertar y actualizar en RestEstudiante

---
+++ src/main/java/com/clearminds/bhfs/servicios/RestEstudiantes.java
@@ -0,0 +1,39 @@
+package com.clearminds.bhfs.servicios;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.clearminds.bhfs.dtos.Estudiante;
+import com.clearminds.bhfs.excepciones.BDDException;
+@Path("/estudiantes")
+public class RestEstudiantes {
+	@Path("/insertar")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertar(Estudiante estudiante){
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(estudiante);
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Path("/actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante estudiante){
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(estudiante);
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
